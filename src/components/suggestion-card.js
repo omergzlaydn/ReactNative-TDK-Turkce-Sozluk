63c4e30600d210186686b03722dab183927a9a4c
omergzlaydn <omrgzlaydn@gmail.com>
2024-01-08 23:06:46 +0300

    -

---
+++ src/components/suggestion-card.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Text from './text';
+import Box from './box';
+import {CardContainer, CardSummary, CardTitle} from './card';
+import LoaderText from './LoaderText';
+
+function SuggestionCard({title, onPress, data, ...props}) {
+  return (
+    <Box {...props}>
+      <Text color="textLight">{title}</Text>
+
+      <CardContainer mt={10} onPress={onPress}>
+        {data ? (
+          <>
+            <CardTitle>{data.madde}</CardTitle>
+            <CardSummary>{data.anlam}</CardSummary>
+          </>
+        ) : (
+          <Box>
+            <LoaderText />
+            <LoaderText width={200} mt={10} />
+          </Box>
+        )}
+      </CardContainer>
+    </Box>
+  );
+}
+
+export default SuggestionCard;
---
+++ src/components/tab-bar.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+import Button from './button';
+import {Search, Bookmark, RotateCcw} from './icons';
+import Box from './box';
+
+import theme from '../utils/theme';
+
+function TabBar({state, descriptors, navigation}) {
+  return (
+    <Box
+      pb={20}
+      bg="white"
+      flexDirection="row"
+      style={{
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowRadius: 20,
+      }}>
+      {state.routes.map((route, index) => {
+        const {options} = descriptors[route.key];
+
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        return label === 'Search' ? (
+          // search button
+          <Box key={label} p={15} mt={-15} bg="white" borderRadius="full">
+            <Button size={56} bg="red" borderRadius="full" onPress={onPress}>
+              <Search stroke="white" />
+            </Button>
+          </Box>
+        ) : (
+          // tab-button
+          <Button
+            key={label}
+            pt={6}
+            flexDirection="column"
+            height={56}
+            flex={1}
+            onPress={onPress}>
+            {label === 'History' && (
+              <RotateCcw
+                color={isFocused ? theme.colors.red : theme.colors.textLight}
+              />
+            )}
+            {label === 'Favorite' && (
+              <Bookmark
+                color={isFocused ? theme.colors.red : theme.colors.textLight}
+              />
+            )}
+
+            {/* indicator */}
+            <Box
+              size={4}
+              bg={isFocused ? 'red' : 'white'}
+              mt={6}
+              borderRadius="full"
+            />
+          </Button>
+        );
+      })}
+    </Box>
+  );
+}
+
+export default TabBar;
